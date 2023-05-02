b26bfa93ff65331cf03bd4c3c9bfa4f1fff9f8a0
Cianuro-83 <buzzeosimone@yahoo.com>
2023-05-02 17:22:56 +0200

    SETUP PROJECT

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="container py-5">as</div>
+    ciao
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@use "./style/general.scss";
+</style>
---
+++ src/components/Template.vue
@@ -0,0 +1,37 @@
+<template></template>
+
+<script>
+export default {
+  components: {
+    // **************************
+    // FINE COMPONETS
+  },
+  // **************************
+  data() {
+    return {
+      // **************************
+      // FINE DATA E RETURN &
+    };
+  },
+  // **************************
+  computed: {
+    // **************************
+    // FINE COMPUTED
+  },
+  // **************************
+  watch: {
+    // **************************
+    // FINE WATCH
+  },
+  // **************************
+  methods: {
+    // **************************
+    // FINE METHODS
+  },
+  // **************************
+};
+</script>
+
+<style lang="scss" scoped>
+@use "../style/partials//variables" as *;
+</style>
