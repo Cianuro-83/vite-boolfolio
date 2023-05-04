e1ce4bab8f1bf9ebab8ee0c21cef86950d8dad05
Cianuro-83 <buzzeosimone@yahoo.com>
2023-05-04 18:06:45 +0200

    FEAT: PAGE SHOW ULTIMATA

--- src/pages/Portfolio.show.vue
+++ src/pages/Portfolio.show.vue
@@ -2,7 +2,28 @@
   <DefaultLayaout>
     <template v-if="project">
       <div class="container">
-        <h1>{{ project.title }}</h1>
+        <h1 class="text-center text-uppercase fw-semibold my-5 text-info">
+          {{ project.title }}
+        </h1>
+        <p class="" v-if="project.type">
+          <span>Tipologia del progetto: </span>
+          <span class="badge text-bg-warning">{{ project.type.name }}</span>
+        </p>
+        <div>
+          <span class="me-2">Tecnologie utilizzate:</span>
+
+          <span v-for="tech in project.technologies" :key="tech.slug">
+            <span
+              class="badge text-bg-success me-3"
+              v-if="project.technologies"
+              >{{ tech.name }}</span
+            >
+          </span>
+        </div>
+      </div>
+
+      <div class="container py-4">
+        <div class="scb" v-html="project.description"></div>
       </div>
     </template>
   </DefaultLayaout>
@@ -38,12 +59,12 @@ export default {
   },
   // **************************
   methods: {
-    fechProjects() {
+    fechProjects(slug) {
       axios
-        .get("http://127.0.0.1:8000/api/projects/${ slug }")
+        .get(`http://127.0.0.1:8000/api/projects/${slug}`)
         .then((res) => {
           let { success, project } = res.data;
-
+          console.log(res.data);
           if (success) {
             this.project = project;
           } else {
@@ -60,11 +81,14 @@ export default {
   },
   // **************************
   created() {
-    this.fechProjects();
+    this.fechProjects(this.slug);
   },
 };
 </script>
 
 <style lang="scss" scoped>
 @use "../style/partials//variables" as *;
+.scb {
+  text-align: justify;
+}
 </style>
